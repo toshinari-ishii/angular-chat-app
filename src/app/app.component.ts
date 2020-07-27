a9434066e43ea85397e1ecf695c0f84de8bd235b
toshinari-ishii <59131143+toshinari-ishii@users.noreply.github.com>
2020-07-27 14:56:41 +0900

    teat3 (#5)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Componen } from '@angular/core';
+import { Component } from '@angular/core';
 import { Comment} from './class/comment';
 import { User} from './class/user';
 
@@ -13,7 +13,7 @@ const COMMENTS: Comment[]=[
   new Comment(CURRENT_USER,"OKです！"),
 ];
 
-@Component({
+@({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
